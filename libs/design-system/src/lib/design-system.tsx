7317f275118b56d8712a5a2eafc8e1f5b1d56ea4
Juanjo <juanjovazpar@gmail.com>
2024-09-25 20:41:01 +0200

    feat(init): Add apps, apis and libraries templates

---
+++ libs/design-system/src/lib/design-system.tsx
@@ -0,0 +1,30 @@
+
+
+
+
+
+  import styled from 'tailwind';
+  
+
+
+const StyledDesignSystem = styled.div`
+  color: pink;
+`;
+
+
+  
+  export function DesignSystem() {
+    return (
+      <StyledDesignSystem>
+        
+        <h1>Welcome to DesignSystem!</h1>
+        
+      </StyledDesignSystem>
+    );
+  }
+  
+
+  export default DesignSystem;
+
+
+
