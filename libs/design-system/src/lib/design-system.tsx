63ef2fd55d1f47ba5adb3212ab0ffd49afc5c6e0
Juanjo <juanjovazpar@gmail.com>
2024-10-04 22:39:21 +0200

    feat(storybook):  Fix storybook

--- libs/design-system/src/lib/design-system.tsx
+++ libs/design-system/src/lib/design-system.tsx
@@ -1,14 +1,8 @@
-import styled from 'tailwind';
 
-const StyledDesignSystem = styled.div`
-  color: pink;
-`;
 
 export function DesignSystem() {
   return (
-    <StyledDesignSystem>
       <h1>Welcome to DesignSystem!</h1>
-    </StyledDesignSystem>
   );
 }
 
