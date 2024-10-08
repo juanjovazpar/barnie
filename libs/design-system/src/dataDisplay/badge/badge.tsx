ed5d2970c78fccf97929aaa02cd3b72d8a033ae1
Juanjo <juanjovazpar@gmail.com>
2024-10-08 22:18:47 +0200

    feat(design-system): Add badge component

--- libs/design-system/src/dataDisplay/badge/badge.tsx
+++ libs/design-system/src/dataDisplay/badge/badge.tsx
@@ -1,3 +1,12 @@
-export const Badge = ({ label, type = 'blue' }) => (
-  <span style={{ backgroundColor: type }}>{label}</span>
+export interface IBadgeProps {
+  label: string;
+  type?: 'square' | 'pill';
+  size?: 'default' | 'xsmall' | 'small' | 'large';
+  color?: 'blue' | 'red' | 'orange' | 'yellow' | 'green' | 'gray';
+}
+
+export const Badge = ({ label, type, size, color = 'blue' }: IBadgeProps) => (
+  <span className={`badge badge-${type} badge-${size} badge-${color}`}>
+    {label}
+  </span>
 );
